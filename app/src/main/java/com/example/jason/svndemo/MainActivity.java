6b58d96b6d4b9569b44aa1f520680e903b93cb69
Jason <320175912@qq.com>
2018-12-19 23:51:10 +0800

    1.0正式版--上线

--- app/src/main/java/com/example/jason/svndemo/MainActivity.java
+++ app/src/main/java/com/example/jason/svndemo/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+/**
+ * 1.0版本稳定版--上线
+ */
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = "Jason";
@@ -14,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.e(TAG,"刚开始开发");
         Log.e(TAG,"1.0版本之前的一系列开发");
+        Log.e(TAG,"1.0稳定版");
     }
 }
