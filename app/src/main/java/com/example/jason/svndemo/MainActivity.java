1664a6e4f9929359248ffe186026e7ab9bf44b4f
Jason <320175912@qq.com>
2018-12-19 23:59:34 +0800

    1.2版本稳定版

--- app/src/main/java/com/example/jason/svndemo/MainActivity.java
+++ app/src/main/java/com/example/jason/svndemo/MainActivity.java
@@ -19,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
         Log.e(TAG,"1.0版本之前的一系列开发");
         Log.e(TAG,"1.0稳定版");
         Log.e(TAG,"1.0基础上修改BUG得到1.1版本");
+        Log.e(TAG,"1.1的基础上修改了BUG得到1.2版本");
     }
 }
